87b03e29c62b18866e96cc90fc921c5fa8d6d89b
snair317 <60349244+snair317@users.noreply.github.com>
2025-07-27 00:51:12 +0530

    Create Volunteer.js

---
+++ src/pages/Volunteer.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Volunteer() {
+  return (
+    <div className="page simple">
+      <h2>Volunteer as a Listener</h2>
+      <p>
+        Want to support others? Anyone can join as a listener.<br/>
+        Just choose <b>Listen</b> on the home page, start a chat, and be there for someone in need.<br/>
+        <b>Tip:</b> Empathy and patience make a huge difference!
+      </p>
+    </div>
+  );
+}
