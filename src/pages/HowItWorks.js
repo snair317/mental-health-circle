db341161a92eaf16fdf18f6202ab26b6b1512b63
snair317 <60349244+snair317@users.noreply.github.com>
2025-07-27 00:47:58 +0530

    Create HowItWorks.js

---
+++ src/pages/HowItWorks.js
@@ -0,0 +1,18 @@
+import React from "react";
+export default function HowItWorks() {
+  return (
+    <div className="page simple">
+      <h2>How It Works</h2>
+      <ol>
+        <li>Enter your name or stay anonymous</li>
+        <li>Choose to <b>Be Heard</b> or <b>Listen</b></li>
+        <li>Start a chat and share or support</li>
+        <li>Feel better knowing you are not alone</li>
+      </ol>
+      <p>
+        Our listeners are here to provide emotional support in a safe and private space.<br/>
+        No registration required. Anonymous. Free.
+      </p>
+    </div>
+  );
+}
